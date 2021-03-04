ef9af844e622aa0a20e27dce44df5f91c9706c7d
t-1120 <r_togai@itccl.jp>
2021-03-04 10:17:20 +0900

    Revert "007:fontawesomeのリンク追加"
    This reverts commit b4b39e84417b6f6ef9cd9c034acaa87e6747dcdf.

--- shop.php
+++ shop.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
     <link rel="stylesheet" href="./css/style.min.css"><!-- 全体css -->
     <link rel="stylesheet" href="./css/menu.min.css">
